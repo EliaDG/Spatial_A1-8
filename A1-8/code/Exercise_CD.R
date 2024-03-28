82e440f234078187457f8ae311e22e11184e73c8
EliaDG <elia.digregorio1492@gmail.com>
2024-03-28 17:06:43 +0100

    Set up Exe.D Rmarkdown

--- A1-8/code/Exercise_CD.R
+++ A1-8/code/Exercise_CD.R
@@ -20,35 +20,35 @@ source("./code/__packages.R")
 
 # Exercise C ------------------------------------------------------------------
 ## 1.C) ------------------------------------------------------------------------
-türkiye <- get_eurostat_geospatial(
+Türkiye <- get_eurostat_geospatial(
   resolution = "01",
   nuts_level = 2,
   year = 2021,
   country = "TR")
 
 #Projection information
-türkiye$geometry
-st_crs(türkiye)
+Türkiye$geometry
+st_crs(Türkiye)
 
-plot_türkiye <- ggplot() +
-  geom_sf(data = türkiye, color = "black", fill = NA) +
+plot_Türkiye <- ggplot() +
+  geom_sf(data = Türkiye, color = "black", fill = NA) +
   theme_minimal() +
   labs(title = "WGS 84 Projection", size = 12)
 
-türkiye_eqearth <- st_transform(türkiye, crs = "+proj=eqearth")
+Türkiye_eqearth <- st_transform(Türkiye, crs = "+proj=eqearth")
 plot_eqearth <- ggplot() +
-  geom_sf(data = türkiye_eqearth, color = "blue", fill = NA) +
+  geom_sf(data = Türkiye_eqearth, color = "blue", fill = NA) +
   theme_minimal() +
   labs(title = "Equal Earth Projection", size = 12)
 
-türkiye_laea <- st_transform(türkiye, crs = "+proj=laea +lat_0=45 +lon_0=30")
+Türkiye_laea <- st_transform(Türkiye, crs = "+proj=laea +lat_0=45 +lon_0=30")
 plot_laea <- ggplot() +
-  geom_sf(data = türkiye_laea, color = "red", fill = NA) +
+  geom_sf(data = Türkiye_laea, color = "red", fill = NA) +
   theme_minimal() +
-  labs(title = "Lambert Azimuthal Equal Area Projection", size = 12)
+  labs(title = "Lambert Azimuthal Projection", size = 12)
 
-combined_plot <- plot_türkiye + plot_eqearth + plot_laea +
-  plot_layout(ncol = 1)+
+TR_plot_1 <- plot_Türkiye + plot_eqearth + plot_laea +
+  plot_layout(ncol = 2) +
   labs(caption="Source: Eurostat")
 
 ## 2.C) ------------------------------------------------------------------------
@@ -59,7 +59,7 @@ data_night <- get_eurostat("tgs00111",
                      TIME_PERIOD = "2022"
                    )) %>% 
   filter(grepl("^TR.*", geo)) %>%
-  merge(., türkiye, by = "geo") %>% 
+  merge(., Türkiye, by = "geo") %>% 
   pivot_wider(., names_from = c_resid, values_from = values) %>% 
   select(-c(2:10, 12:15)) %>% 
   mutate(DOM_SHARE = DOM/TOTAL,
@@ -74,49 +74,51 @@ centroids_df <- st_coordinates(centroids) %>%
 data_night <- cbind(data_night, centroids_df)
 
 # Discrete Plot
-TR_plot_1 <- ggplot(data_night) +
+TR_plot_2 <- ggplot(data_night) +
   geom_sf(aes(fill = TURIST, geometry = geometry)) +
   geom_text(data = data_night[data_night$TURIST == "Foreign Tourist", ], aes(x = x, y = y, label = NUTS_NAME),
             size = 3, color = "black", nudge_y = 0.1) +
   theme_map() +
   labs(x = NULL, y = NULL,
        title = "Distribution of Tourist Overnight-Stay by Origin",
-       subtitle = "Turkey - NUTS2 Level",
+       subtitle = "Türkiye - NUTS2 Level",
        caption = "Source: Eurostat")+
   guides(fill = guide_legend(title = "Tourist Type:")) +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        plot.title = element_text(face = "bold"))
 
 # Continuous Plot
-TR_plot_2 <- ggplot(data_night) +
+TR_plot_3 <- ggplot(data_night) +
   geom_sf(aes(fill = DOM_SHARE, geometry = geometry)) +
-  geom_text_repel(data = data_night[data_night$DOM_SHARE > 0.9, ],
+  geom_text_repel(data = data_night[data_night$DOM_SHARE > 0.95, ],
                   aes(x = x, y = y, label = NUTS_NAME),
                   size = 3, color = "black", nudge_y = 0.1,
                   box.padding = 0.5,
-                  arrow = arrow(length = unit(0.75, "mm") , ends = "last", type = "closed")) +
+                  arrow = arrow(length = unit(0.75, "mm") , ends = "last", type = "open")) +
   theme_map() +
   labs(x = NULL, y = NULL,
        title = "Distribution of Domestic Tourist Overnight-stay",
-       subtitle = "Turkey - NUTS2 Level",
+       subtitle = "Türkiye - NUTS2 Level",
        caption = "Source: Eurostat") +
-  theme(legend.position = "bottom") +
+  theme(legend.position = "bottom",
+        plot.title = element_text(face = "bold")) +
   scale_fill_viridis(option = "viridis",
                      direction = 1, 
                      name = "Share of Domestic Tourist Overnights",
                      guide = guide_colorbar(direction = "horizontal", 
                                             barheight = unit(2, units = "mm"),
-                                            barwidth = unit(30, units = "mm"),
+                                            barwidth = unit(54, units = "mm"),
                                             draw.ulim = TRUE,
                                             title.position = "top"))
 
 ## 3.C) ------------------------------------------------------------------------
-#Raster-based or Vector-based, see R_mardown file
+#Raster-based or Vector-based, see R_markdown file
 
 # Final Plots Exercise C -------------------------------------------------------
-combined_plot
+
 TR_plot_1
 TR_plot_2
-
+TR_plot_3
 
 # Exercise D -------------------------------------------------------------------
 ## 1.D) ------------------------------------------------------------------------
@@ -137,10 +139,10 @@ PL_plot_1 <- ggplot(df) +
        caption = "Source: PKW") +
   guides(fill=guide_legend(title = "Winner:")) +
   theme(
-    plot.title = element_text(size = 16, face = "bold"),
-    plot.subtitle = element_text(size = 14),
-    legend.title = element_text(size = 14),
-    legend.text = element_text(size = 12))
+    plot.title = element_text(size = 11, face = "bold"),
+    plot.subtitle = element_text(size = 10),
+    legend.title = element_text(size = 10),
+    legend.text = element_text(size = 10))
 
 ## 2.D) ------------------------------------------------------------------------
 data <- pol_pres15 %>%
@@ -169,7 +171,7 @@ data_with_centroid <- cbind(data, st_coordinates(centroid))
 
 PL_plot_2 <- ggplot(data_with_centroid) +
   geom_sf(fill = "white") +
-  geom_point(data = subset(data_with_centroid, anomaly > 0), aes(x = X, y = Y, color = as.factor(anomaly_type), size = as.factor(anomaly)), alpha = 0.8) +
+  geom_point(data = subset(data_with_centroid, anomaly > 0), aes(x = X, y = Y, color = as.factor(anomaly_type), size = as.factor(anomaly)), alpha = 0.6) +
   theme_void() +
   labs(title = "2015 Polish Presidential Election: anomalies in PVE",
        subtitle = "Poland - Municipality Level",
@@ -177,22 +179,20 @@ PL_plot_2 <- ggplot(data_with_centroid) +
        color = "Anomaly Type:",
        size = "Anomaly Count:") +
   theme(
-    plot.title = element_text(size = 16, face = "bold"),
-    plot.subtitle = element_text(size = 14),
-    legend.title = element_text(size = 14),
-    legend.text = element_text(size = 12))
+    plot.title = element_text(size = 11, face = "bold"),
+    legend.text = element_text(size = 10))
 
 ## 3.D) ------------------------------------------------------------------------
 ds <- pol_pres15 %>%
   pivot_longer(cols = c(I_turnout, II_turnout), names_to = "election", values_to = "turnout")
 
 #attempt to customize palette for borders
-border_colors <- c("Rural" = "red", "Urban" = "yellow", "Urban/rural" = "green", "Warsaw Borough" = "purple")
+#border_colors <- c("Rural" = "red", "Urban" = "yellow", "Urban/rural" = "green", "Warsaw Borough" = "purple")
 
 PL_plot_3 <- tm_shape(ds) +
   tm_fill(col = "turnout", title = "Turnout Share:", palette = "plasma" ) +
-  tm_borders(group = ds$types, alpha = 0.8) +
-  tm_facets(by = c("election"), free.scales = FALSE) +
+  tm_borders(invisible()) +
+  tm_facets(by = "election", free.scales = FALSE) +
   tm_layout(main.title = "Turnout Comparison between I and II round",
             title.position = c("center", "top"),
             frame = TRUE) +
